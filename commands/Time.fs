9019083f3d6bb98169b219774e14fa6c353b7fdb
EluciusFTW <guysbuss@gmail.com>
2023-08-07 15:24:18 +0200

    WIP integrating worldtimeapi

--- commands/Time.fs
+++ commands/Time.fs
@@ -2,14 +2,31 @@ namespace Commands
 
 open Spectre.Console.Cli
 open SpectreCoff
+open FSharp.Data
+
+module Time = 
+    [<Literal>]
+    let locationUrl = "http://worldtimeapi.org/api/timezone/Europe/Stockholm"
+    [<Literal>]
+    let baseUrl = "http://worldtimeapi.org/api/timezone"
+
+    type LocationResponse = JsonProvider<baseUrl>
+    type TimeResponse = JsonProvider<locationUrl>
 
 type Time() =
     inherit Command<Settings.LocationSettings>()
     interface ICommandLimiter<Settings.LocationSettings>
 
     override _.Execute(_context, settings) = 
-        Many [
-            C $"The time in {settings.city} is "; 
-            P "still unknown."
+
+        let zones = Time.LocationResponse.Load(Time.baseUrl)
+        let zone = 
+            "Which location are you interested in?" 
+            |> chooseFrom zones 
+        
+        let zoneTime = Time.TimeResponse.Load($"{Time.baseUrl}/{zone})")
+        Many [  
+            C $"The time in {zone} is "; 
+            P $"{zoneTime.Unixtime}"
         ] |> toConsole
         0
